73fc1654687285439ac33f734ebc834d5e2f8a05
Archie Baer <archie@abaer.dev>
2021-02-18 21:51:17 +0000

    URL Navigation

---
+++ navigation.js
@@ -0,0 +1,15 @@
+browser.webNavigation.onCompleted.addListener(async ({ url }) => {
+    const { disableNavigation, api, token } = await browser.storage.local.get();
+
+    try {
+        const { protocol, hostname } = new URL(url);
+        if (protocol !== "https:" || !hostname || disableNavigation || true) return;
+        // TODO: Add option to enable/disable sharing websites with Alles
+
+        axios.post(
+            `${api || `https://protium.alles.cx/c`}/url`,
+            { domain: hostname },
+            { headers: { Authorization: token } }
+        ).catch(() => { });
+    } catch (err) { }
+});
